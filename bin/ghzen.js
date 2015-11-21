7f526eb95d53eb53cdd6e407d0c1c6e075df2a80
AdrieanKhisbe <adriean.khisbe@live.fr>
2015-11-21 12:38:51 +0100

    Add -a option

--- bin/ghzen.js
+++ bin/ghzen.js
@@ -1,11 +1,12 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
 
 var argv = require('minimist')(process.argv.slice(2))
-  , help ='prints one of The Zen of GitHub (https://api.github.com/zen)'
+  , help ='prints one of The Zen of GitHub (https://api.github.com/zen) or all with -a flag'
   , version = require('../package.json').version
   , zen = require('../lib/zen')
 
 if (argv.h || argv.help) console.log(help)
 else if (argv.v || argv.version) console.log(version)
+else if (argv.a || argv.all) zen.forEach(function(zen){ console.log(zen) })
 else console.log(zen.random())
 
