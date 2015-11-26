1026cbfe3f16b89a7e9fa0b368123df2a1eea027
Talles L <talleslasmar@gmail.com>
2015-11-26 00:50:05 -0200

    simplified man page and README

--- bin/ghzen.js
+++ bin/ghzen.js
@@ -1,12 +1,11 @@
 #!/usr/bin/env node
 
 var argv = require('minimist')(process.argv.slice(2))
-  , help ='prints one of The Zen of GitHub (https://api.github.com/zen) or all with -a flag'
-  , version = require('../package.json').version
+  , pkg = require('../package.json')
   , zen = require('../lib/zen')
 
-if (argv.h || argv.help) console.log(help)
-else if (argv.v || argv.version) console.log(version)
+if (argv.h || argv.help) console.log(pkg.description)
+else if (argv.v || argv.version) console.log(pkg.version)
 else if (argv.a || argv.all) zen.forEach(function(zen){ console.log(zen) })
 else console.log(zen.random())
 
